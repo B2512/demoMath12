6903209f7dcca03b7b9b620365fb7f7593429d8f
SAIH <1914432057@qq.com>
2019-10-23 23:27:44 +0800

    Add huangsai

---
+++ src/b2512/huangsai/Main.java
@@ -0,0 +1,19 @@
+package b2512.huangsai;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = new String[]{"ffaom","fflfd","ffom"};
+        String minstr = names[0];
+        for (int i =0;i<names.length;i++){
+            System.out.println(names[i].indexOf(minstr));
+            if (minstr.length()>names[i].length()){
+                minstr = names[i];
+            }
+            while (names[i].indexOf(minstr)!=0){ // 比较 indexOf: 0相等  -1不相等
+                minstr = minstr.substring(0,minstr.length()-1); //substring:截取
+            }
+        }
+        System.out.println(minstr);
+    }
+}
