24d0417fc0b97a43390e165f28318fea3654385b
SAIH <1914432057@qq.com>
2019-10-24 14:55:42 +0800

    Add huangsai

--- src/b2512/huangsai/Main.java
+++ src/b2512/huangsai/Main.java
@@ -6,10 +6,11 @@ public class Main {
         String[] names = new String[]{"ffaom","fflfd","ffom"};
         String minstr = names[0];
         for (int i =0;i<names.length;i++){
-            System.out.println(names[i].indexOf(minstr));
-            if (minstr.length()>names[i].length()){
-                minstr = names[i];
-            }
+//            System.out.println(names[i].indexOf(minstr));
+////            //从最短开始比较
+////            if (minstr.length()>names[i].length()){
+////                minstr = names[i];
+////            }
             while (names[i].indexOf(minstr)!=0){ // 比较 indexOf: 0相等  -1不相等
                 minstr = minstr.substring(0,minstr.length()-1); //substring:截取
             }
